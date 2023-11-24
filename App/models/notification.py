18382ac4e3b4612be281aeb226a499cf5328f244
BitNinja101 <fralakaimank@gmail.com>
2023-11-24 19:12:54 -0400

    updated notification table

--- App/models/notification.py
+++ App/models/notification.py
@@ -2,16 +2,12 @@ from datetime import datetime
 from App.database import db
 
 class Notification(db.Model):
+    __tablename__ = 'notification'
+    
     id = db.Column(db.Integer, primary_key=True)
-    competitor_id = db.Column(db.Integer, db.ForeignKey('competitors.id'))
+    competitor_id = db.Column(db.Integer, db.ForeignKey('competitor.id'))
     message = db.Column(db.String)
     timeStamp=db.Column(db.DateTime,defualt = datetime.utcnow)
-
-    
-    def __init(self,competitor_id, message, timeStamp):
-        self.competitor_id = competitor_id
-        self.message = message
-        self.timeStamp = timeStamp or datetime.utcnow()
     
     def toDict(self):
      return{
