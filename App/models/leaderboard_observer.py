5d3414edb689180388e1c01f0cedb5eba06147e3
Allyy02 <sampath.ally@gmail.com>
2023-11-26 21:20:38 +0000

    update leaderboard_observer

--- App/models/leaderboard_observer.py
+++ App/models/leaderboard_observer.py
@@ -1,6 +1,8 @@
-from App.database import db, abstractmethod
+from App.database import db
+from abc import abstractmethod
 
 class LeaderBoardObserver(db.Model):
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
     
     @abstractmethod
     def update(self):
