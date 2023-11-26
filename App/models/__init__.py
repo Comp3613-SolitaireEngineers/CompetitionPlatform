422c5d0033c44e1c775bf31221c60d843a6e3120
Allyy02 <sampath.ally@gmail.com>
2023-11-26 22:45:00 +0000

    delete Leaderboard models

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -2,11 +2,8 @@ from .Rank import *
 from .User import *
 from .Admin import *
 from .Host import *
-from .LeaderboardObserver import *
-from .LeaderboardPublisher import *
 from .Competition import *
 from .CompetitionHost import *
 from .Competitor import *
-from .Leaderboard import *
 from .UserCompetition import *
 
