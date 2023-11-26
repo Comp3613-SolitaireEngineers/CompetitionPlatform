42c7b82da76c4d10371469df6d963fc555f2ea63
Allyy02 <sampath.ally@gmail.com>
2023-11-26 21:24:49 +0000

    update names of model files

--- App/models/UserCompetition.py
+++ App/models/UserCompetition.py
@@ -1,6 +1,7 @@
 from App.database import db
 
 class UserCompetition(db.Model):
+    __tablename__ = 'usercompetition'
     id = db.Column(db.Integer, primary_key=True)
     comp_id = db.Column(db.Integer, db.ForeignKey('competition.id'), nullable=False)
     user_id =  db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
