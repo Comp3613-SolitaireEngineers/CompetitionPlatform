2b3335222c0127246ba6c5b78cf7078383eaa70e
Devon Murray <104084504+devxlag@users.noreply.github.com>
2023-11-28 15:49:57 +0000

    rank routes

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -4,7 +4,8 @@ from .user import user_views
 from .index import index_views
 from .auth import auth_views
 from .competition import comp_views
+from .rank import rank_views
 
 
-views = [user_views, index_views, auth_views, comp_views] 
+views = [user_views, index_views, auth_views, comp_views, rank_views]  
 # blueprints must be added to this list
---
+++ App/views/rank.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from flask_jwt_extended import jwt_required, current_user as jwt_current_user
+from flask_login import current_user, login_required
+from App.controllers import *
+
+rank_views = Blueprint('rank_views', __name__, template_folder='../templates')
+
+@rank_views.route('/api/ranks', methods=['GET'])
+def api_get_ranks():
+    ranks = get_all_ranks_json()
+    if ranks:
+        return (jsonify(ranks),200)
+    return jsonify({'message': 'No Ranks found'}), 405
+    
+@rank_views.route('/api/rank/<competitor_id>', methods=['GET'])
+def api_get_competitor_rank(competitor_id):
+    rank = get_rank_by_competitor_id(competitor_id)
+    if rank:
+        return jsonify(rank.get_json()), 200
+    else:
+        return jsonify({'message': 'Competitor Rank not found'}), 405
+    
+@rank_views.route('/api/rank/ranking', methods=['PUT'])
+def api_update_rank():
+    data = request.json
+    rank = update_rank(competitor_id=data["competitor_id"], ranking=data['ranking'])
+    if rank:
+        return jsonify({'message': 'Rank updated successfully'}), 200
+    else:
+        return jsonify({'error': 'Rank not updated'}), 400
+    
+@rank_views.route('/api/rank/points', methods=['PUT'])
+def api_update_rank_points():
+    data = request.json
+    rank = update_rank_points(competitor_id=data["competitor_id"] , points=data['points'])
+    if rank:
+        return jsonify({'message': 'Rank points updated successfully'}), 200
+    else:
+        return jsonify({'error': 'Rank point not updated'}), 400
