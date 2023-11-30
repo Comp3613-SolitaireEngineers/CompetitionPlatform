23b6437265670c4351cf98684ee893d5f6c948d8
Ally <98034572+Allyy02@users.noreply.github.com>
2023-11-30 19:11:03 -0400

    add call for rank command in execute function

--- App/models/ResultsCommand.py
+++ App/models/ResultsCommand.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 from App.database import db
+from App.models import Command, RankCommand
+from App.controllers import results
+from App.controllers import get_admin, get_competition_by_id
 
 class ResultsCommand(Command):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,19 +16,31 @@ class ResultsCommand(Command):
     def get_json(self):
         return{
             'id': self.id,
-            'competition_id': self.competition_id, 
-            "executed_at": self.executed_at
+            'competition_id': self.competition_id
         }
+
+    def toDict(self):
+        res = {
+            "id": self.id,
+            "competition_id": self.competition_id,
+            "executed_at": self.executed_at,
+        } 
+        return res
     
     def execute(self, admin_id, competition_id, results):
         
         admin = get_admin(admin_id)
-        competition = get_competition(competition_id)
+        competition = get_competition_by_id(competition_id)
         
         if admin:
             if competition:
-                admin.add_results(competition_id, results)
-                admin.update_ranks()
+                results.add_results(competition_id, results)
+                
+                # Create and execute a RankCommand for each competitor
+                for competitor in competition.participants:
+                    new_rank = calculate_new_rank(competitor)  
+                    command = RankCommand(competitor, new_rank)
+                    command.execute()
             else:
                 return ("Unable to add results")
         else:
