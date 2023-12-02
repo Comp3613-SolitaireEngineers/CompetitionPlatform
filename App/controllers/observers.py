1dc8423141ed67735f0f5315e9a5a394c9f9600b
Allyy02 <98034572+Allyy02@users.noreply.github.com>
2023-12-02 13:44:31 -0400

    add controllers for observers

---
+++ App/controllers/observers.py
@@ -0,0 +1,19 @@
+from App.models import RankTopObservers
+from App.database import db
+
+def create_rank_top_observers(name):
+    try:
+        rank_top_observer = RankTopObservers.RankTopObservers(name)
+        db.session.add(rank_top_observer)
+        db.session.commit()
+        return rank_top_observer
+    except Exception as e:
+        print("Error creating rank_top_observers " + str(e))
+        
+    return None
+
+def get_rank_top_observers_by_name(name):
+    return RankTopObservers.query.filter_by(name=name).first()
+
+def get_rank_top_observers_by_id(id):
+    return RankTopObservers.query.get(id)
