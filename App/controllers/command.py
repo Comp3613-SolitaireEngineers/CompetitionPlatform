e036654111c28bb309b48aa6c9c72dd1cd1a9dfe
Allyy02 <98034572+Allyy02@users.noreply.github.com>
2023-12-01 23:49:25 -0400

    update competition command controllers

--- App/controllers/command.py
+++ App/controllers/command.py
@@ -1,22 +1,27 @@
-from App.models import Command
+from App.models import Command, CompetitionCommand, ResultsCommand, RankCommand, UpdateRanksCommand, PointsCommand, Admin
 from App.database import db
 
+def create_competition_command(self, competition_id):
+    command = CompetitionCommand(competition_id)
+    
+    try:
+        db.session.add(command)
+        db.session.commit()
+        return command
+    except Exception as e:
+        print (f"Error creating competition : {str(e)}")
+        
+    return None
+    
 def execute_competition_command(self, admin_id, competition_id):
+    
+    admin = Admin.query.get(admin_id)
     command = CompetitionCommand(competition_id)
     try:
         command.execute(admin_id, competition_id)
         return "Competition command executed successfully"
     except Exception as e:
         return f"Error executing competition command: {str(e)}"
-    
-def execute_results_command(self, admin_id, competition_id, results):
-    command = ResultsCommand(competition_id)
-    try:
-        command.execute(admin_id, competition_id, results)
-        return "Results command executed successfully"
-    except Exception as e:
-        return f"Error executing results command: {str(e)}"
-
 
 def get_competition_command_by_id(command_id):
     command = CompetitionCommand.query.get(command_id)
