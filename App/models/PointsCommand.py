38bb9c3d9a7d86d78d49162c3471b2fce4a8ba8f
Allyy02 <98034572+Allyy02@users.noreply.github.com>
2023-12-03 13:25:53 -0400

    update points command

--- App/models/PointsCommand.py
+++ App/models/PointsCommand.py
@@ -6,7 +6,7 @@ from sqlalchemy import func
 class PointsCommand(Command):
     __tablename__ = 'points_command'
     id = db.Column(db.Integer, primary_key=True)
-    competitor_id = db.Column(db.Integer, db.ForeignKey('competitor.id'))
+    competitor_id = db.Column(db.String(220), db.ForeignKey('competitor.id'))
     executed_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 
