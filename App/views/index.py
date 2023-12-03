be4d1201dc16d279da707ee2f423e63bdcba995f
Allyy02 <98034572+Allyy02@users.noreply.github.com>
2023-12-03 13:42:01 -0400

    update index

--- App/views/index.py
+++ App/views/index.py
@@ -8,8 +8,8 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 @index_views.route('/', methods=['GET'])
 def index_page():
     page = request.args.get('page', 1, type=int)
-    competitors = leaderboard_competitors(page)
-    return render_template('leaderboard.html', competitors=competitors, page=page)
+    # competitors = leaderboard_competitors(page)
+    return render_template('leaderboard.html') #, competitors=competitors, page=page)
 
 @index_views.route('/init', methods=['GET'])
 def init():
