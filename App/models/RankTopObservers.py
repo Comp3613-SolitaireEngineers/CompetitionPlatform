7873e15c0d93aef127fab6d056a0261d756f6bd3
devonxmurray <devon.murray@my.uwi.edu>
2023-12-03 23:02:38 -0400

    integration tests updated

--- App/models/RankTopObservers.py
+++ App/models/RankTopObservers.py
@@ -66,11 +66,11 @@ class RankTopObservers(db.Model):
         latest_rank_command = RankCommand.query.filter_by(competitor_id=competitor.id).order_by(RankCommand.executed_at.desc()).first()
         if latest_rank_command:
             if latest_rank_command.old_rank == 0:
-                competitor.update_notifications("You are a new competitor and have entered the top 20")
+                competitor.update_notifications("You are a new competitor and have entered the top 20", "Congragtulations")
             else:
-                competitor.update_notifications(message.format(latest_rank_command.old_rank, latest_rank_command.new_rank))
+                competitor.update_notifications(message.format(latest_rank_command.old_rank, latest_rank_command.new_rank), "Rank Update")
         else:
-            competitor.update_notifications("You have entered the top 20")
+            competitor.update_notifications("You have entered the top 20", "Congragtulations")
 
 
     def get_json(self):
